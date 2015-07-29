1117991d976261282e0c602d28232c6a706762c2
dwy <dominic.wyler@zuehlke.com>
2015-07-29 09:30:24 +0200

    initial commit

---
+++ BowlingKata/BowlingKata.Test/GameTest.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BowlingKata.Test
+{
+    public class GameTest
+    {
+        private Game game;
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            game = new Game();
+        }
+
+        [Test]
+        public void InitialScoreShouldBeZero()
+        {
+            Assert.AreEqual(0, game.Score());
+        }
+
+        [Test]
+        public void StrikesOnlyShouldReturn300()
+        {
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+            game.Roll(10);
+
+            Assert.AreEqual(300, game.Score());
+        }
+
+        [Test]
+        public void PairsOfNineAndMissShouldReturn90()
+        {
+            game.Roll(9);
+            game.Roll(0);
+
+            game.Roll(9);
+            game.Roll(0);
+
+            game.Roll(9);
+            game.Roll(0);
+
+            game.Roll(9);
+            game.Roll(0);
+
+            game.Roll(9);
+            game.Roll(0);
+
+            game.Roll(9);
+            game.Roll(0);
+
+            game.Roll(9);
+            game.Roll(0);
+
+            game.Roll(9);
+            game.Roll(0);
+
+            game.Roll(9);
+            game.Roll(0);
+
+            game.Roll(9);
+            game.Roll(0);
+
+            Assert.AreEqual(90, game.Score());
+        }
+
+        [Test]
+        public void FivesOnlyShouldReturn150()
+        {
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            
+            Assert.AreEqual(150, game.Score());
+        }
+
+        [Test]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void RollingMoreThen21TimesShouldThrowException()
+        {
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(5);
+        }
+    }
+}
