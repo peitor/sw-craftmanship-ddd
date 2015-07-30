713550408f39678148d6bef3491a4060917a1597
dwy <dominic.wyler@zuehlke.com>
2015-07-30 12:49:34 +0200

    introduced parameter rolls
    preparation for extracting class from parameters

--- BowlingKata/BowlingKata/Game.cs
+++ BowlingKata/BowlingKata/Game.cs
@@ -16,46 +16,46 @@
             int frameIndex = 0;
             for (int frame = 0; frame < 10; frame++)
             {
-                if (IsStrike(frameIndex))
+                if (IsStrike(frameIndex, rolls))
                 {
-                    score += 10 + StrikeBonus(frameIndex);
+                    score += 10 + StrikeBonus(frameIndex, rolls);
                     frameIndex++;
                 }
-                else if (IsSpare(frameIndex))
+                else if (IsSpare(frameIndex, rolls))
                 {
-                    score += 10 + SpareBonus(frameIndex);
+                    score += 10 + SpareBonus(frameIndex, rolls);
                     frameIndex += 2;
                 }
                 else
                 {
-                    score += SumOfBallsInFrame(frameIndex);
+                    score += SumOfBallsInFrame(frameIndex, rolls);
                     frameIndex += 2;
                 }
             }
             return score;
         }
 
-        private bool IsStrike(int frameIndex)
+        private bool IsStrike(int frameIndex, int[] rolls)
         {
             return rolls[frameIndex] == 10;
         }
 
-        private bool IsSpare(int frameIndex)
+        private bool IsSpare(int frameIndex, int[] rolls)
         {
             return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
         }
 
-        private int StrikeBonus(int frameIndex)
+        private int StrikeBonus(int frameIndex, int[] rolls)
         {
             return rolls[frameIndex + 1] + rolls[frameIndex + 2];
         }
 
-        private int SpareBonus(int frameIndex)
+        private int SpareBonus(int frameIndex, int[] rolls)
         {
             return rolls[frameIndex + 2];
         }
 
-        private int SumOfBallsInFrame(int frameIndex)
+        private int SumOfBallsInFrame(int frameIndex, int[] rolls)
         {
             return rolls[frameIndex] + rolls[frameIndex + 1];
         }
