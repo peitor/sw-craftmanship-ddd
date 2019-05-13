699688691c154cfd9cae4862489b1c5cc7c794b2
Peter Gfader <peter@gfader.com>
2019-05-13 14:13:57 +0200

    Move more Given to base

--- BowlingKata/BowlingKata.Test/TestCommons.cs
+++ BowlingKata/BowlingKata.Test/TestCommons.cs
@@ -1,4 +1,7 @@
-﻿namespace BowlingKata.Test
+﻿using System;
+using FakeItEasy;
+
+namespace BowlingKata.Test
 {
     public class TestCommons
     {
@@ -6,6 +9,13 @@
         {
             var game = new Game();
             return game;
+        }
+
+        public static Action<GameFinishedData> GivenEventFinishedHandler(Game game)
+        {
+            var gameFinishedHandler = A.Fake<Action<GameFinishedData>>();
+            game.GameFinished += gameFinishedHandler;
+            return gameFinishedHandler;
         }
     }
 }
