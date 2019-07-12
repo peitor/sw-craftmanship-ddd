832e562848cccdd7bbd9904a9254b73e5b212d2e
Peter Gfader <peter@gfader.com>
2019-07-12 10:32:53 +0200

    added comment and todo

--- BowlingKata/BowlingKata/HallOfFame/HallOfFame.cs
+++ BowlingKata/BowlingKata/HallOfFame/HallOfFame.cs
@@ -4,8 +4,17 @@ namespace BowlingKata.HallOfFame
 {
     public class HallOfFame
     {
+        /// <summary>
+        /// How many top games are in the hall of fame?
+        /// There is a Maximum.
+        /// </summary>
         public int Length => new HallOfFameRepository().GetTopGames(3).Count();
 
+        /// <summary>
+        /// Return the details of that game in the hall of fame.
+        /// TODO: Better GetHallOfFame returns in order the top games. Avoid the this.
+        /// Smell: Tell don't ask.
+        /// </summary>
         public BowlingGame this[int position] => new HallOfFameRepository().GetAllGames()[position];
     }
 }
