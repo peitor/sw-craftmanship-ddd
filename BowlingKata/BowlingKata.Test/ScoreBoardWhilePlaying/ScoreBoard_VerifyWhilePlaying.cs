930640cc16ab8471a2b1dd3c3aae29c93ff85fc5
Peter Gfader <peter@beyond-agility.com>
2021-08-03 15:26:57 +0200

    r Game_CurrentScore --> moved to WhilePlaying

--- BowlingKata/BowlingKata.Test/ScoreBoardWhilePlaying/ScoreBoard_VerifyWhilePlaying.cs
+++ BowlingKata/BowlingKata.Test/ScoreBoardWhilePlaying/ScoreBoard_VerifyWhilePlaying.cs
@@ -1,4 +1,6 @@
-﻿using BowlingKata.ScoreBoardWhilePlaying;
+﻿using System;
+using BowlingKata.ScoreBoardWhilePlaying;
+using Commons;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -49,5 +51,186 @@ namespace BowlingKata.Test.ScoreBoardWhilePlaying
             scoreboard["Sepp"].CurrentScore().Should().Be(4);
         }
         
+        
+         [Test]
+        public void RunningGame_ReturnsMinus1()
+        {
+            Scoreboard scoreboard = new Scoreboard();
+            var game = scoreboard["1stGame"];
+            game.Roll(3);
+            game.Roll(5);
+
+            game.Roll(5);
+
+            game.CurrentScore().Should().Be(13);
+        }
+
+        [Test]
+        public void StrikesOnly_ShouldReturn300()
+        {
+            Scoreboard scoreboard = new Scoreboard();
+            var game = scoreboard["1stGame"];
+            12.Times(() => game.Roll(10));
+
+            game.CurrentScore().Should().Be(300);
+        }
+
+        [Test]
+        public void PairsOfNineAndMissShouldReturn90()
+        {
+            Scoreboard scoreboard = new Scoreboard();
+            var game = scoreboard["1stGame"];
+            10.Times(() =>
+            {
+                game.Roll(9);
+                game.Roll(0);
+            });
+
+            game.CurrentScore().Should().Be(90);
+        }
+
+        [Test]
+        public void FivesOnlyShouldReturn150()
+        {
+            Scoreboard scoreboard = new Scoreboard();
+            var game = scoreboard["1stGame"];
+
+            21.Times(() => game.Roll(5));
+
+            game.CurrentScore().Should().Be(150);
+        }
+
+        [Test]
+        public void OneNormalRoll()
+        {
+            Scoreboard scoreboard = new Scoreboard();
+            var game = scoreboard["1stGame"];
+
+            RollAndAssert(game, 3, 3);
+        }
+
+        [Test]
+        public void TwoNormalRolls()
+        {
+            Scoreboard scoreboard = new Scoreboard();
+            var game = scoreboard["1stGame"];
+
+            RollAndAssert(game, 3, 3);
+            RollAndAssert(game, 4, 7);
+        }
+
+        [Test]
+        public void TODO_ToVerify()
+        {
+            // TODO
+            Scoreboard scoreboard = new Scoreboard();
+            var g = scoreboard["1stGame"];
+
+            RollAndAssert(g, 10, 10);
+
+            RollAndAssert(g, 1, 12);
+            Assert.Inconclusive("Shouldn't this be 10 since the frame is not completed?");
+
+            RollAndAssert(g, 0, 12);
+        }
+
+        [Test]
+        public void OneStrike_2Empty()
+        {
+            Scoreboard scoreboard = new Scoreboard();
+            var g = scoreboard["1stGame"];
+
+            RollAndAssert(g, 10, 10);
+
+            RollAndAssert(g, 1, 12);
+            RollAndAssert(g, 1, 14);
+
+            RollAndAssert(g, 0, 14);
+        }
+
+
+        [Test]
+        public void SimulateFullGame_2Strikes_LotsOr0()
+        {
+            Scoreboard scoreboard = new Scoreboard();
+            var g = scoreboard["1stGame"];
+
+            RollAndAssert(g, 10, 10);
+            RollAndAssert(g, 0, 10);
+            RollAndAssert(g, 0, 10);
+
+            RollAndAssert(g, 2, 12);
+            RollAndAssert(g, 3, 15);
+
+            RollAndAssert(g, 10, 25);
+
+            RollAndAssert(g, 5, 35);
+            RollAndAssert(g, 5, 45);
+
+            RollAndAssert(g, 2, 49);
+            RollAndAssert(g, 0, 49);
+
+            RollAndAssert(g, 0, 49);
+            RollAndAssert(g, 0, 49);
+
+            RollAndAssert(g, 0, 49);
+            RollAndAssert(g, 0, 49);
+        }
+
+        [Test]
+        public void SimulateGame_WithStrikes_And_LotsOfEmpty()
+        {
+            // Simulation validated by http://www.sportcalculators.com/bowling-score-calculator
+
+            Scoreboard scoreboard = new Scoreboard();
+            var game = scoreboard["1stGame"];
+            game.Roll(10);
+
+            game.Roll(0);
+            game.Roll(0);
+
+            game.Roll(2);
+            game.Roll(3);
+
+            game.Roll(10);
+
+            game.Roll(5);
+            game.Roll(5);
+
+            game.Roll(2);
+            game.Roll(0);
+
+            game.Roll(0);
+            game.Roll(0);
+
+            game.Roll(0);
+            game.Roll(0);
+
+            game.Roll(0);
+            game.Roll(0);
+
+            game.Roll(0);
+            game.Roll(0);
+
+            game.CurrentScore().Should().Be(49);
+        }
+
+
+        [Test]
+        public void RollingMoreThen21TimesShouldThrowException()
+        {
+            Scoreboard scoreboard = new Scoreboard();
+            var game = scoreboard["1stGame"];
+
+            Action act = () => 22.Times(() => game.Roll(5));
+
+            act.Should().Throw<IndexOutOfRangeException>();
+        }
+
+        private static void RollAndAssert(ScoreWhilePlayingGame g, int pins, int expectedCurrentScore)
+        {
+            g.Roll(pins);
+            g.CurrentScore().Should().Be(expectedCurrentScore);
+        }
     }
 }
